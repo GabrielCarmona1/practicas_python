bce5e7927e2fda3fcadbb479b18dfc662d67ea0d
GabrielCarmona1 <carmona.gar.angel@gmail.com>
2022-02-26 17:31:32 -0600

    manejo de variable

---
+++ manejoDeVariables.py
@@ -0,0 +1,16 @@
+#MAnejo de variables
+texto = "Hola pantera"
+nombre = "Gabriel Carmona"
+altura = "1.72"
+year = 2022
+v_error = texto + str (year)
+
+#impresiones
+print (f"{texto} -  {nombre} \n \t  {altura}  -  {year} ")
+print (texto,year)
+print (v_error)
+
+
+#Entrada
+datoEntrada = input("¿Cual es tu edad?: ")
+print ( "Tu edad es: " + str(datoEntrada) + " 19años")
